f24248c105da91786c061fa2b2eac182dda6c671
Muhammad Sabbir Khan Alif <muhammadsabbirkhanalif@gmail.com>
2024-11-24 08:41:13 +0000

    Explore woment's fashion section added in the homepage

---
+++ src/Components/Women_collection_section.jsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import React from 'react'
+import womenCollection from '../../public/women_collection.jpg'
+import { MdArrowOutward } from "react-icons/md";
+
+export default function Women_collection_section() {
+  return (
+    <div className='lg:flex lg:flex-row flex flex-col-reverse items-center lg:justify-center mt-32 lg:space-x-6 px-10'>
+        <Image className='rounded-lg shadow-xl mt-4 lg:mt-0' src={womenCollection} alt='collection'/>
+        <div className='space-y-4'>
+            <h1 className='text-4xl text-center lg:text-start'>Explore the <span className='text-[#6F42C1]'>Elegance</span> of Women's Fashion</h1>
+            <p className='text-gray-500 font-semibold text-center lg:text-start'>Discover a curated selection of timeless styles, trendy outfits, and versatile essentials designed to elevate your wardrobe.</p>
+            <div className='flex items-center justify-center lg:justify-start'>
+                <button className="px-4 py-3 text-white rounded-badge bg-[#8F71E1]">Shop Now</button>
+                <button className="p-3 text-white rounded-full bg-[#8F71E1]"><MdArrowOutward size={24} /></button>
+            </div>
+        </div>
+    </div>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Banner from "@/Components/Banner";
 import DiscountSection from "@/Components/DiscountSection";
 import Featured_products_section from "@/Components/Featured_products_section";
 import Features_section from "@/Components/Features_section";
+import Women_collection_section from "@/Components/Women_collection_section";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Features_section />
       <Featured_products_section />
       <DiscountSection />
+      <Women_collection_section />
     </div>
   );
 }
